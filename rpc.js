36386eabb68ae6e665126255adb8e806c0543f08
rAlexB <ruben.barbeiro12@gmail.com>
2025-12-14 22:25:34 +0000

    Function to read users input is implemented
    getHumanChoice receives and treats the user input with treatGameInput, functions are tested

--- rpc.js
+++ rpc.js
@@ -10,3 +10,16 @@ function getComputerChoice () {
     }
     return play;
 }
+
+function getHumanChoice () {
+    let humanChoice = prompt("What is your play? Rock, Papper or Scissors?");
+    humanChoice = treatGameInput(humanChoice);
+
+    return humanChoice;
+}
+
+function treatGameInput (input) {
+    input = input.toLowerCase();
+    input = input.replace(input.at(0), input.at(0).toUpperCase());
+    return input;
+}
